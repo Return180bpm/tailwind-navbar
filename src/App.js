09e4ac4c6630fb5744774654ecf1b30316235d5d
Tom Szwaja <th.szwaja@gmail.com>
2022-01-13 17:32:26 +0100

    feat: Create SideBar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { SideBar } from "./SideBar";
 
 function App() {
-    return <div className="flex"></div>;
+    return (
+        <div className="flex">
+            <SideBar />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/SideBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const SideBar = () => {
+    return (
+        <div className="fixed left-0 top-0 h-screen flex flex-col bg-slate-900 text-white"></div>
+    );
+};
